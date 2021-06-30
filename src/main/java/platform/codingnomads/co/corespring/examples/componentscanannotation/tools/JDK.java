3e75cf3047e841992c6b98a2b4b1f86b194b6ff0
Jared Larsen <jaredjoel@me.com>
2021-06-29 17:47:33 -0700

    section 7 - example refactors

---
+++ src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/tools/JDK.java
@@ -0,0 +1,7 @@
+package platform.codingnomads.co.corespring.examples.componentscanannotation.tools;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JDK {
+}
--- src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/weapon/Framework.java
+++
@@ -1,8 +0,0 @@
-package platform.codingnomads.co.corespring.examples.componentscanannotation.weapon;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Framework {
-}
-
