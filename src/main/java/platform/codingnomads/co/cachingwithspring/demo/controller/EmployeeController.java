240f0eb5c277e4eccb420c73426417c492e5d633
Ryan Desmond <ryan@codingnomads.co>
2022-10-28 12:29:27 -0700

    updating caching from controller to service

--- src/main/java/platform/codingnomads/co/cachingwithspring/demo/controller/EmployeeController.java
+++ src/main/java/platform/codingnomads/co/cachingwithspring/demo/controller/EmployeeController.java
@@ -1,10 +1,6 @@
 package platform.codingnomads.co.cachingwithspring.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import platform.codingnomads.co.cachingwithspring.demo.model.Employee;
@@ -19,12 +15,9 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @Autowired
-    CacheManager cacheManager;
-
     @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeService.saveEmployee(employee);
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.saveEmployee(employee));
     }
 
     @GetMapping("/employees")
@@ -33,24 +26,21 @@ public class EmployeeController {
     }
 
     @GetMapping("employees/{employeeId}")
-    @Cacheable(value = "employees", key = "#employeeId", sync = true)
-    public Employee findEmployeeById(@PathVariable(value = "employeeId") Integer employeeId) {
-        System.out.println("Employee fetching from database: " + employeeId);
-        return employeeService.getEmployee(employeeId);
+    public ResponseEntity<Employee> findEmployeeById(@PathVariable(value = "employeeId") Integer employeeId) {
+        return ResponseEntity.ok(employeeService.getEmployee(employeeId));
     }
 
     @PutMapping("employees/{employeeId}")
-    @CachePut(value = "employees", key = "#employeeId")
-    public Employee updateEmployee(@PathVariable(value = "employeeId") Integer employeeId,
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "employeeId") Integer employeeId,
                                    @RequestBody Employee employeeDetails) {
         Employee employee = employeeService.getEmployee(employeeId);
         employee.setName(employeeDetails.getName());
-        return employeeService.saveEmployee(employee);
+        return ResponseEntity.ok(employeeService.saveEmployee(employee));
     }
 
     @DeleteMapping("employees/{id}")
-    @CacheEvict(value = "employees", allEntries = true)
-    public void deleteEmployee(@PathVariable(value = "id") Integer employeeId) {
+    public ResponseEntity<Object> deleteEmployee(@PathVariable(value = "id") Integer employeeId) {
         employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok(new Object());
     }
 }
--- src/main/java/platform/codingnomads/co/cachingwithspring/demo/service/EmployeeService.java
+++ src/main/java/platform/codingnomads/co/cachingwithspring/demo/service/EmployeeService.java
@@ -1,6 +1,9 @@
 package platform.codingnomads.co.cachingwithspring.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import platform.codingnomads.co.cachingwithspring.demo.model.Employee;
 import platform.codingnomads.co.cachingwithspring.demo.exception.ResourceNotFoundException;
@@ -14,20 +17,45 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
+//    can be used to inspect the cache during debug
+//    @Autowired
+//    CacheManager cacheManager;
+
+    @Cacheable(value = "employees", key = "#employeeId", sync = true)
     public Employee getEmployee(Integer employeeId) {
+
+        fakeDelay("Fetching employee from the database...");
+
         return employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResourceNotFoundException("Employee not found: " + employeeId));
     }
 
+    @Cacheable(value = "employees")
     public List<Employee> getAllEmployees() {
+        fakeDelay("Fetching all employees from the database...");
         return employeeRepository.findAll();
     }
 
+    @CachePut(value = "employees", key = "#employee.id")
     public Employee saveEmployee(Employee employee) {
+        System.out.println("Saving user to the database and updating cache");
         return employeeRepository.save(employee);
     }
 
+    @CacheEvict(value = "employees", allEntries = true)
     public void deleteEmployee(Integer employeeId) {
+        System.out.println("Removing all users from the database and cache");
         employeeRepository.deleteById(employeeId);
+    }
+
+    public void fakeDelay(String message){
+        for (int i = 0; i < 5; i++){
+            System.out.println(message);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
