f992614ffb510ea61ea2f9bdf791e013c0924d65
Danny Brown <xaerr00@gmail.com>
2023-01-19 00:56:45 -0800

    HTML/CSS/JS Tests

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,16 @@
 function greeting() {
     alert("Hello Spring Developer!!");
 }
+document.getElementById("mouseOver").addEventListener("mouseover", mouseOver);
+document.getElementById("mouseOver").addEventListener("mouseout", mouseOut);
 
+function mouseOver() {
+  document.getElementById("mouseOver").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("mouseOver").style.color = "black";
+}
+function currentTime() {
+  document.getElementById("displayDate").innerHTML = Date();
+}
